e79c6df7a37697bacb1277ca1a55625c5ae2d06b
Sabrina <sabrinamedwinter@gmail.com>
2021-05-10 13:13:26 -0500

    added tabbed interface

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,6 +4,7 @@ import {CalculatorControls} from './CalculatorControls';
 import {PaymentGraph} from './PaymentGraph';
 import {PaymentTable} from './PaymentTable';
 import { handleMortgageDataChange } from './utils';
+import { Tabs } from './Tabs';
 
 
 export const Calculator = () => {
@@ -39,15 +40,7 @@ export const Calculator = () => {
 				interestRate={interestRate}
 				setInterestRate={setInterestRate}
 			/>
-
-			<PaymentGraph
-				mortgageTerm={mortgageTerm}
-				yearlyPayments={yearlyPayments}
-			/>
-			<PaymentTable
-				amountToBorrow={amountToBorrow}
-				yearlyPayments={yearlyPayments}
-			/>
+			
 		</>
 	);
 
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -5,19 +5,47 @@ import './App.css';
 
 function Tabs() {
 
-    <div className="container">
-        <div className="bloc-tabs">
-            <div className="tabs active-tabs">Amortization Chart</div>
-            <div className="tabs">Interest Repayment Chart</div>
-            <div className="tabs">Amortization Table</div>
-        </div>
+    const[toggleState, setToggleState ] = useState(1);
 
-        <div className="content-tabs">
-            <div className="content active-content">
+        const toggletab = (index) => {
+            setToggleState(index);
+        }
 
+        return (
+
+        <div className="container">
+            <div className="bloc-tabs">
+                <button
+                    className="tabs active-tabs"
+                    onClick={() => toggleTab(1)}>
+                    Amortization Chart</button>
+                <button 
+                    className="tabs"
+                    onClick={() => toggleTab(2)}>
+                    Amortization Table</button>
             </div>
-            <div className="content-tab"></div>
-            <div className="content-tab"></div>
+
+                <div 
+                className={ toggleState===1 ? "content active-content" : "content" }
+                onClick={() => toggleTab(1)}>
+                    <div 
+                    className="content active-content">
+                        <PaymentGraph
+                            mortgageTerm={mortgageTerm}
+                            yearlyPayments={yearlyPayments}
+                        />
+                    </div>
+                    <div 
+                    className={ toggleState===2 ? " content active-content" : "content"}
+                    onClick={() => toggleTab(2)} >
+                        <PaymentTable
+                        amountToBorrow={amountToBorrow}
+                        yearlyPayments={yearlyPayments}
+                    />
+                </div>
+            </div>
         </div>
-    </div>
+    );
 }
+
+export default Tabs;
