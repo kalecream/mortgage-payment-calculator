aa26501906d18c8b610de899e1fe46093b0bcddd
Sabrina <sabrinamedwinter@gmail.com>
2021-05-12 05:47:12 -0500

    undo tabs

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,9 +1,69 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Tab from './Tab';
+import { Calculator } from './Calculator';
 import {PaymentGraph} from './PaymentGraph';
 import {PaymentTable} from './PaymentTable';
 import './App.css';
 
-function Tabs() {
+
+class Tabs extends Component {
+    static propTypes = {
+        children: PropTypes.instanceOf(Array).isRequired,
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeTab: this.props.children[0].props.label,
+        };
+    }
+
+    onClickTabItem = (tab) => {
+        this.setState({ activeTab: tab});
+    }
+
+    render() {
+        const {
+            onClickTabItem,
+            props: { 
+                children, 
+            },
+            state: {
+                activeTab,
+            }
+        } = this;
+
+        return ( 
+            <div className="tabs">
+                <ol className="tab-list">
+                    {children.map((child) => {
+                        const { label } = child.props;
+
+                        return (
+                            <Tab 
+                                activeTab = {activeTab}
+                                key = {label}
+                                label = {label}
+                                onClick={onClickTabItem}
+                            />
+                        );
+                    })}
+                </ol>
+                <div className="tab-content">
+                    {children.map((child) => {
+                        if (child.props.label !== activeTab) return undefined;
+                        return child.pros.children;
+                    })}
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export const Tabs = () => {
 
     const[toggleState, setToggleState ] = useState(1);
 
